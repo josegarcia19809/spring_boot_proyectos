b5b1de50fc8bdaf4e31626bc998ad1b45ddf6684
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-24 20:47:52 -0600

    Usando rutas y páginas web

---
+++ paginas_web/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.ico.paginas_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String mostrarIndex(){
+        return "index";
+    }
+}
